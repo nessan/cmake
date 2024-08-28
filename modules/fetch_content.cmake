486a4aacd9e6c39878015db58d8064c8e9b7cb06
nessan <nessan.fitzmaurice@me.com>
2024-08-28 18:04:33 +0100

    removed a couple of message(...) calls

--- modules/fetch_content.cmake
+++ modules/fetch_content.cmake
@@ -9,12 +9,10 @@ function(fetch_content)
     include(FetchContent)
     set(FETCHCONTENT_QUIET FALSE)
     set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE ALWAYS)
-    message("<fetch_content> Fetching: ${ARGV}")
     FetchContent_Declare(${ARGV}
         GIT_SHALLOW 1
         GIT_PROGRESS TRUE
         SYSTEM
     )
-    message("<fetch_content> Making available: ${ARGV0}")
     FetchContent_MakeAvailable(${ARGV0})
 endfunction()
